6c62e906a267d066909e700307e1b664f60a8c68
djordjeavr <djordjeavram5@gmail.com>
2020-07-29 15:24:28 +0200

    Update

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var app =express();
 const path = require('path');
 var port =process.env.PORT || 3000;
 
-app.use(express.static('index.html'));
+app.use(express.static(__dirname + '/dist/'));
 app.get('/',function(req,res){
-    res.sendFile(path.join('index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 
 })
 app.listen(port);
